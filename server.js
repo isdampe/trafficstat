eb6c8def3c09e23531d5bb848bceaadae5edb3af
Richard Denton <isdampe@gmail.com>
2015-05-20 15:13:32 +1000

    First commit

---
+++ server.js
@@ -0,0 +1,60 @@
+var http = require("http");
+var url = require('url');
+var pageRequests = 0;
+var requestsLastMin = 0;
+var minStart = new Date().getTime() / 1000;
+
+http.createServer(function(req, res){
+	
+	var buff, j, perSecond, cd, d, m;
+	var queryData = url.parse(req.url, true);
+	var path, q;
+	
+	path = queryData.pathname;
+	q = queryData.query;
+	
+	
+	if ( path === "/proc" ) {
+		
+		if ( q.hasOwnProperty("reqs") ) {
+			pageRequests = pageRequests + parseInt(q.reqs);
+			requestsLastMin = requestsLastMin + parseInt(q.reqs);
+		} else {
+			pageRequests++;
+			requestsLastMin++;	
+		}
+		
+		res.end();
+
+	} else if ( path === "/stat" ) {
+		
+		cd = Math.round(new Date().getTime() / 1000, 0);
+		d = Math.round(cd - minStart, 0);
+		m = Math.round(60 / d, 0);
+		perSecond = Math.round(( requestsLastMin * m ) / 60, 0);
+			
+		j = {
+			count: pageRequests,
+			cps: perSecond
+		};
+		
+		buff = JSON.stringify(j);
+		
+		res.writeHead(200,{
+			'Content-Length': buff.length,
+			'Content-Type': 'text/json'
+		});
+		res.write(buff);
+		res.end();
+		
+	} else {
+		res.writeHead(403);
+		res.end();
+	}
+	
+}).listen(8001);
+
+setInterval(function(){
+	minStart = new Date().getTime() / 1000;
+	requestsLastMin = 0;
+},60000);
